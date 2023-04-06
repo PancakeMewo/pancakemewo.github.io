0709de7772215435902c0b6538e64266149d0746
PancakeMewo <128839891+PancakeMewo@users.noreply.github.com>
2023-04-07 03:58:51 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ form.addEventListener('submit', (e) => {
 
     e.preventDefault()
 })
+
+function accountSwitchTabs() {
+    const buttonList = document.getElementsByClassName('account-button');
+    const detailsList = document.getElementsByClassName('details');
+    for (let index = 0; index < buttonList.length; index++) {
+        if (buttonList[index].classList.contains('inactive')) {
+            buttonList[index].classList.replace('inactive', 'active');
+            detailsList[index].classList.replace('inactive', 'active');
+        } else {
+            buttonList[index].classList.replace('active', 'inactive');
+            detailsList[index].classList.replace('active', 'inactive');
+        }
+    }
+}
